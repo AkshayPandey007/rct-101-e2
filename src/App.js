291f8287643d3b738cc47f423e7fb19b1d20bf88
AkshayPandey007 <101665005+AkshayPandey007@users.noreply.github.com>
2022-05-30 16:37:58 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+// import Products from "./components/Products";
+
+import Products from "./components/Products";
 
 const App = () => {
-  return <div>{/* TODO: Code here */}</div>;
+
+
+  return <div>{/* TODO: Code here */}
+  <Products />
+  </div>;
+
 };
 
 export default App;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React,{useEffect} from "react";
+import axios from "axios"
+import { useState } from "react";
 
 const Products = () => {
   // TODO: Remove below const and instead import them from chakra
   const Flex = () => <div />;
   const Grid = () => <div />;
+  
+  const[lists,setList]=useState([])
+
+  useEffect(()=>{
+    const getList = async()=>{
+      let r = await axios.get(" http://localhost:8080/products")
+   
+      console.log(r.data)
+      setList(r.data)
+    }
+    getList()
+  },[])
 
   return (
     <Flex>
       {/*  AddProduct */}
-      <Grid>{/* List of Products */}</Grid>
+      <Grid>
+    {/* List of Products */}</Grid>
       {/* Pagination */}
     </Flex>
   );
